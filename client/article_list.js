b0662f22726e2219560d4891bb11eb164f30ecd9
Desingh rajan <mailme@desinghrajan.in>
2015-02-24 07:31:52 +0530

    Adding articles collection

--- client/article_list.js
+++ client/article_list.js
@@ -1,9 +1,9 @@
 Template.articlesList.helpers({
-   articles: function (){
-       return [
-           {title: "Hello world", body:"I am saying hello to the world!"},
-           {title: "How are you", body:"Hope you are doing well" },
-           {title:"This is sample article", body: "You can type anything inside this article because it's just for test"},
-       ];
-   }
+    articles: function () {
+        return [
+            {title: "Hello world", body: "I am saying hello to the world!"},
+            {title: "How are you", body: "Hope you are doing well" },
+            {title: "This is sample article", body: "You can type anything inside this article because it's just for test"},
+        ];
+    }
 });
